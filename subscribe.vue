9c213e712ee25449048c9a256eea9e5db718f5c1
jazuly <ajajazuly@gmail.com>
2020-08-29 00:56:12 +0700

    Update subscribe.vue

--- subscribe.vue
+++ subscribe.vue
@@ -1,38 +1,107 @@
 <template>
-    <div>
-        <section>
-            <b-row>
-                <b-col cols="2" class="side-menu">
-                <!-- <SideProfile /> -->
-                </b-col>
-
-                <b-col cols="10">
-                    <div id="component-container"></div>
-                </b-col>
-            </b-row>
-        </section>
+  <div>
+    <div id="payment-page">
+      <div class="container">
+        <div v-if="type === 'dropin'" id="dropin" ref="dropin"></div>
+        <div v-else class="payment-container">
+          <div id="card" class="payment" :ref="`${type}`"></div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-export default {
-    name: 'profile-subscription2',
 
-    head: {
-        titleTemplate: "Profile Subscription2 | %s",
-        meta: [
-            { hid: "description", name: "description", content: "Profile Subscription2 Page" }
-        ],
-        link: [
-        { rel: "stylesheet", type: "text/css", href: "https://checkoutshopper-test.adyen.com/checkoutshopper/sdk/3.10.1/adyen.css" }
-        ],
-        script: [
-        { src: "https://checkoutshopper-test.adyen.com/checkoutshopper/sdk/3.10.1/adyen.js" }
-        ],
-    },
-
-    mounted() {
-
-    }
+function handleSubmission(state, component, url) {
+  callServer(url, state.data)
+    .then(res => handleServerResponse(res, component))
+    .catch(error => {
+      throw Error(error);
+    });
 }
+function callServer(url, data) {
+  return fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(res => res.json());
+}
+function handleServerResponse(res, component) {
+  if (res.action) {
+    component.handleAction(res.action);
+  } else {
+    switch (res.resultCode) {
+      case "Authorised":
+        window.location.href = "/success";
+        break;
+      case "Pending":
+        window.location.href = "/pending";
+        break;
+      case "Refused":
+        window.location.href = "/failed";
+        break;
+      default:
+        window.location.href = "/error";
+        break;
+    }
+  }
+}
+export default {
+  head() {
+    return {
+      title: "Payment page"
+    };
+  },
+  asyncData({ route }) {
+    return { type: route.params.payment };
+  },
+  data() {
+    return {
+      clientKey: "",
+      response: ""
+    };
+  },
+  mounted() {
+    $nuxt.$axios.get('/payment2/listmethods')
+      .then(data => {
+        this.clientKey = '';
+        this.response = data.data;
+        console.log(data)
+        const configuration = {
+          paymentMethodsResponse: this.response,
+          clientKey: this.clientKey,
+          locale: "en_US",
+          environment: "test",
+          showPayButton: true,
+          paymentMethodsConfiguration: {
+            ideal: {
+              showImage: true
+            },
+            card: {
+              hasHolderName: true,
+              holderNameRequired: true,
+              name: "Credit or debit card",
+              amount: {
+                value: 1000,
+                currency: "EUR"
+              }
+            }
+          },
+          onSubmit: (state, component) => {
+            handleSubmission(state, component, "/api/initiatePayment");
+          },
+          onAdditionalDetails: (state, component) => {
+            handleSubmission(state, component, "/api/submitAdditionalDetails");
+          }
+        };
+        const checkout = new AdyenCheckout(configuration);
+        const integration = checkout
+          .create(this.type)
+          .mount(this.$refs[this.type]);
+      });
+  }
+};
 </script>
